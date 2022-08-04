793b8405c89a890733441c2f221381ffc1fceb82
shirisha peddaboina <106797889+shirishapeddaboina@users.noreply.github.com>
2022-08-04 09:43:21 +0530

    Delete index.js

--- Redux/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import ProductReducer from './ProductReducer';
-
-const rootReducer = combineReducers({
-    ProductReducer
-})
-
-export default rootReducer;
-
