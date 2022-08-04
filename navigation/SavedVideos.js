c2ed8e9a2601b8c6a9a20a1aa8a0e062dbe0d7a1
shirisha peddaboina <106797889+shirishapeddaboina@users.noreply.github.com>
2022-08-04 10:08:04 +0530

    Add files via upload

---
+++ navigation/SavedVideos.js
@@ -0,0 +1,28 @@
+ import * as React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+const SavedVideos = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1, padding: 16}}>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 25,
+              textAlign: 'center',
+              marginBottom: 16,
+            }}>
+            Videos
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+ 
+export default SavedVideos;
